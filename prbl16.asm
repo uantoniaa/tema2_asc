fc863442ab6f975df0a684d70027c9c8594c1c60
Antonia Ungureanu <93488180+uantoniaa@users.noreply.github.com>
2022-05-24 20:31:41 +0300

    Create prbl16.asm

---
+++ prbl16.asm
@@ -0,0 +1,39 @@
+; nasm -f elf sqrt.asm -o sqrt.o
+; gcc sqrt.o -o sqrt
+
+default rel
+extern printf
+extern scanf
+
+section .data
+    format_output:   	db "%f", 0xa, 0
+    format_input:	db "%lf", 0
+	
+section .bss
+	res:	resq 1
+	x:		resq 1
+
+section .text
+    global main
+main:
+    sub    rsp, 8
+	
+	lea 	rsi, [x]
+    lea 	rdi, [format_input]
+    mov 	rax, 1
+	call scanf	wrt ..plt
+	
+	fld qword [x]  
+
+	fsqrt
+
+	fstp qword [res]
+	
+	movq 	xmm0, qword [res]
+    mov 	rdi, format_output
+    mov 	rax, 1
+    call   printf  wrt ..plt
+
+    add    rsp, 8
+    xor    eax, eax
+    ret
